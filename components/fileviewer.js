1c5434526d5154a4ce50d6ce4264a75571c947da
abdullahbq <abdullahbinqueyam@gmail.com>
2023-11-20 13:39:26 +0530

    Update

--- components/fileviewer.js
+++ components/fileviewer.js
@@ -4,7 +4,14 @@ class FileViewer extends HTMLElement {
         this.data = {};
     }
 
-    async connectedCallback() {
+    connectedCallback() {
+        this.setupSidebar();
+        this.loadFolderData();
+        window.addEventListener('load', () => this.responsiveSidebar());
+        window.addEventListener('resize', () => this.responsiveSidebar());
+    }
+
+    async loadFolderData() {
         try {
             const response = await fetch('sidebar.json');
             if (!response.ok) {
@@ -17,70 +24,62 @@ class FileViewer extends HTMLElement {
         }
     }
 
-    async loadFileContent(filePath) {
-        try {
-            const response = await fetch(filePath);
-            if (response.ok) {
-                return await response.text();
-            } else {
-                console.error("Failed to load file content:", response.status, response.statusText);
-                return "Failed to load content";
-            }
-        } catch (error) {
-            console.error("Error fetching the file:", error);
-            return "Failed to load content";
-        }
+    setupSidebar() {
+        const sidebar = document.createElement('div');
+        sidebar.id = 'docs-sidebar';
+        document.body.appendChild(sidebar);
     }
 
-    async renderFolder(folder, folderName) {
+    responsiveSidebar() {
+        const sidebar = document.getElementById('docs-sidebar');
+        sidebar.classList.toggle('sidebar-hidden', window.innerWidth < 1200);
+        sidebar.classList.toggle('sidebar-visible', window.innerWidth >= 1200);
+    }
+
+    renderFolder(folder, folderName) {
         const filesContainer = this.querySelector(`#${folderName} .files`);
-        const filesHTML = folder.map((fileName, fileIndex) => `<li data-file="${fileIndex}" data-folder="${folderName}" class="file p-1" style="cursor: pointer">${fileName.replace('.html', '')}</li>`).join("");
+
+        const filesHTML = folder.map((fileName, fileIndex) =>
+            `<li id="${fileIndex}" data-file="${fileIndex}" data-folder="${folderName}" class="file" style="cursor: pointer">${fileName.replace('.html', '')}</li>`
+        ).join("");
+
         filesContainer.innerHTML = filesHTML;
 
-        const fileElements = this.querySelectorAll('.file');
-
-        fileElements.forEach((fileElement) => fileElement.addEventListener('click', () => this.handleFileClick(fileElement.dataset.file, fileElement.dataset.folder)));
+        filesContainer.addEventListener('click', (event) => {
+            const fileElement = event.target.closest('.file');
+            if (fileElement) {
+                const { file, folder } = fileElement.dataset;
+                this.handleFileClick(file, folder);
+            }
+        });
     }
 
-    async render() {
-        const defaultContent = "Select a file to view its content.";
+    render() {
+        const defaultContent = '<div><title-component title="Welcome to our Courses"></title-component></title-component><div class="container py-3"><h2>We offer tutorials for various courses.</h2><p>Click a topic from the sidebar to start reading the topic.</p></div></div>';
         this.innerHTML = `
-        <section class="courses-section bg-primary bg-opacity-10">
-        <title-component title="Courses"></title-component>
-        <div class="container-fluid py-2">
-                <div class="row">
-                    <div class="col-md-4" style="max-height: 90vh; overflow-y: auto;">
-                        <div class="accordion" id="fileAccordion">
-                            ${Object.keys(this.data).map((folderName, folderIndex) => `
-                                <div class="accordion-item" id="${folderName}">
-                                    <h2 class="accordion-header" id="folder${folderIndex}Header">
-                                        <button class="accordion-button py-2 collapsed bg-success bg-opacity-10" type="button" data-bs-toggle="collapse" data-bs-target="#folder${folderIndex}" aria-expanded="false" aria-controls="folder${folderIndex}">
-                                            ${folderName}
-                                        </button>
-                                    </h2>
-                                    <div id="folder${folderIndex}" class="accordion-collapse collapse" aria-labelledby="folder${folderIndex}Header" data-bs-parent="#fileAccordion">
-                                        <div class="accordion-body p-0">
-                                            <ul class="files pt-2"></ul>
-                                        </div>
-                                    </div>
-                                </div>
-                            `).join('')}
-                        </div>
-                    </div>
-                    <div class="col-md-8">
-                        <div class="content">
-                            <div class="content-container">
-                                <h2>Courses we Offer</h2>
-                                <p>${defaultContent}</p>
+            <section class="courses-section">                         
+                <div id="docs-sidebar" class="docs-sidebar bg-primary bg-opacity-10">                       
+                    ${Object.keys(this.data).map((folderName, folderIndex) => `
+                        <div id="${folderName}">
+                            <button id="${folderName}" class="w-100 h-20 d-flex align-items-center border-1 my-1" style="font-size: 1.05rem; font-weight: 600; height: 38px" data-bs-toggle="collapse" data-bs-target="#folder${folderIndex}">
+                                <i class="fas fa-folder me-2"></i>
+                                <span>${folderName}</span>
+                            </button>                                   
+                            <div id="folder${folderIndex}" class="collapse">
+                                <ul class="files"></ul>                                       
                             </div>
                         </div>
-                    </div>         
+                    `).join('')}                     
                 </div>
-            </div>
+                <div class="docs-content">             
+                    <div class="content-container">
+                        ${defaultContent}
+                    </div> 
+                </div>                                
             </section>
-            `;
+        `;
 
-        Object.keys(this.data).forEach((folderName) => this.renderFolder(this.data[folderName], folderName));
+        Object.keys(this.data).forEach((folderName, folderIndex) => this.renderFolder(this.data[folderName], folderName, folderIndex));
     }
 
     async handleFileClick(fileIndex, folderName) {
@@ -100,7 +99,7 @@ class FileViewer extends HTMLElement {
                 const title = doc.querySelector('title').innerText;
                 const bodyContent = doc.querySelector('body').innerHTML;
 
-                contentContainer.innerHTML = `<h2>${title}</h2><div>${bodyContent}</div>`;
+                contentContainer.innerHTML = `<title-component title="${title}"></title-component><div class="container py-3">${bodyContent}</div>`;
             } else {
                 console.error("Failed to load file content:", response.status, response.statusText);
                 contentContainer.innerHTML = "Failed to load content";
