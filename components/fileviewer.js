367fa344876b2b4694590511f65e124978a4dd4d
abdullahbq <abdullahbinqueyam@gmail.com>
2023-11-21 00:50:20 +0530

    Update

--- components/fileviewer.js
+++ components/fileviewer.js
@@ -2,12 +2,13 @@ class FileViewer extends HTMLElement {
     constructor() {
         super();
         this.data = {};
+        this.debouncedResize = this.debounce(() => this.responsiveSidebar(), 200); // Debounce resize event
     }
 
     connectedCallback() {
         this.setupSidebar();
         window.addEventListener('load', () => this.responsiveSidebar());
-        window.addEventListener('resize', () => this.responsiveSidebar());
+        window.addEventListener('resize', this.debouncedResize); // Use debounced resize event
     }
 
     async loadFolderData() {
@@ -32,14 +33,18 @@ class FileViewer extends HTMLElement {
 
     responsiveSidebar() {
         const sidebar = document.getElementById('docs-sidebar');
-        sidebar.classList.toggle('sidebar-hidden', window.innerWidth < 1200);
-        sidebar.classList.toggle('sidebar-visible', window.innerWidth >= 1200);
+        if (sidebar) {
+            sidebar.classList.toggle('sidebar-hidden', window.innerWidth < 1200);
+            sidebar.classList.toggle('sidebar-visible', window.innerWidth >= 1200);
+        }
     }
 
     renderFolder(folder, folderName) {
         const filesContainer = this.querySelector(`#${folderName} .files`);
-        const filesHTML = folder.map((fileName, fileIndex) =>
-            `<li id="${fileIndex}" data-file="${fileIndex}" data-folder="${folderName}" class="file mb-2" style="cursor: pointer">${fileName.replace('.html', '')}</li>`
+        const filesHTML = folder.map((fileName, fileIndex) => `
+            <li id="${fileIndex}" data-file="${fileIndex}" data-folder="${folderName}" class="file mb-2" style="cursor: pointer">
+                ${fileName.replace('.html', '')}
+            </li>`
         ).join("");
         filesContainer.innerHTML = filesHTML;
         filesContainer.addEventListener('click', (event) => {
@@ -52,29 +57,39 @@ class FileViewer extends HTMLElement {
     }
 
     render() {
-        const defaultContent = '<div><title-component title="Welcome to our Courses"></title-component></title-component><div class="container py-3"><h2>We offer tutorials for various courses.</h2><p>Click a topic from the sidebar to start reading the topic.</p></div></div>';
-        this.innerHTML = `
-            <section class="courses-section">                         
-                <div id="docs-sidebar" class="docs-sidebar bg-primary bg-opacity-10">                       
-                    ${Object.keys(this.data).map((folderName, folderIndex) => `
-                        <div id="${folderName}">
-                            <button id="${folderName}" class="w-100 h-20 d-flex align-items-center border-1 my-1" style="font-size: 1.05rem; font-weight: 600; height: 38px" data-bs-toggle="collapse" data-bs-target="#folder${folderIndex}">
-                                <i class="fas fa-folder me-2"></i>
-                                <span>${folderName}</span>
-                            </button>                                   
-                            <div id="folder${folderIndex}" class="collapse">
-                                <ul class="files"></ul>                                       
-                            </div>
-                        </div>
-                    `).join('')}                     
-                </div>
-                <div class="docs-content">             
-                    <div class="content-container">
-                        ${defaultContent}
-                    </div> 
-                </div>                                
-            </section>
-        `;
+        const defaultContent = `<div>
+            <title-component title="Welcome to our Courses"></title-component>
+            <div class="container py-3">
+                <h2>We offer tutorials for various courses.</h2>
+                <p>Click a topic from the sidebar to start reading the topic.</p>
+            </div>
+        </div>`;
+        this.innerHTML = `<section class="courses-section">                         
+            <div id="docs-sidebar" class="docs-sidebar bg-primary bg-opacity-10">                       
+                ${Object.keys(this.data).map((folderName, folderIndex) => `
+                    <div id="${folderName}">
+                        <button
+                            data-folder-name="${folderName}"
+                            data-folder-index="${folderIndex}"
+                            class="w-100 h-20 d-flex align-items-center border-1 my-1"
+                            style="font-size: 1.05rem; font-weight: 600; height: 38px"
+                            data-bs-toggle="collapse"
+                            data-bs-target="#folder${folderIndex}">
+                            <i class="fas fa-folder me-2"></i>
+                            <span>${folderName}</span>
+                        </button>                                   
+                        <div id="folder${folderIndex}" class="collapse">
+                            <ul class="files"></ul>                                       
+                        </div>                    
+                    </div>
+                `).join('')}                     
+            </div>
+            <div class="docs-content">             
+                <div class="content-container">
+                    ${defaultContent}
+                </div> 
+            </div>                                
+        </section>`;
         Object.keys(this.data).forEach((folderName, folderIndex) => this.renderFolder(this.data[folderName], folderName, folderIndex));
     }
 
@@ -89,8 +104,8 @@ class FileViewer extends HTMLElement {
                 const htmlContent = await response.text();
                 const parser = new DOMParser();
                 const doc = parser.parseFromString(htmlContent, 'text/html');
-                const title = doc.querySelector('title').innerText;
-                const bodyContent = doc.querySelector('body').innerHTML;
+                const title = doc.querySelector('title')?.innerText || 'Untitled';
+                const bodyContent = doc.querySelector('body')?.innerHTML || '';
                 contentContainer.innerHTML = `<title-component title="${title}"></title-component><div class="container py-3">${bodyContent}</div>`;
             } else {
                 console.error("Failed to load file content:", response.status, response.statusText);
@@ -100,6 +115,26 @@ class FileViewer extends HTMLElement {
             console.error("Error fetching the file:", error);
             contentContainer.innerHTML = "Failed to load content";
         }
+    }
+
+    // Debounce function to limit the rate at which a function can fire
+    debounce(func, wait, immediate) {
+        let timeout;
+        return function executedFunction() {
+            const context = this;
+            const args = arguments;
+
+            const later = function () {
+                timeout = null;
+                if (!immediate) func.apply(context, args);
+            };
+
+            const callNow = immediate && !timeout;
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+
+            if (callNow) func.apply(context, args);
+        };
     }
 }
 
